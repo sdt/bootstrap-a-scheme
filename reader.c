7fb005e583328b2a3598ecdae31ed3ea62be892f
Stephen Thirlwall <sdt@dr.com>
2015-04-18 13:52:01 +1000

    Partly working reader.
    My whole in-place string business is totally broken...

--- reader.c
+++ reader.c
@@ -1,34 +1,45 @@
 #include "reader.h"
 
+#include "debug.h"
 #include "exception.h"
+#include "types.h"
 
 #include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define ARRAY_SIZE(a)   (sizeof(a)/sizeof((a)[0]))
 
 typedef struct {
-    char* input;
-    char* cursor;
+    char*       cursor;
+    const char* token;
 } Tokeniser;
 
-static void skip_ws(Tokeniser* t)
+static void skipSpace(Tokeniser* t)
 {
     while (isspace(*t->cursor)) {
         t->cursor++;
     }
 }
 
-static int tokeniser_eof(Tokeniser* t)
+static int strEq(const char* s, const char* t)
 {
-    skip_ws(t);
-    return *t->cursor == 0;
+    return strcmp(s, t) == 0;
 }
 
-static const char* tokeniser_next(Tokeniser* t)
+static int tokeniser_eof(Tokeniser* t)
 {
-    if (tokeniser_eof(t)) {
-        return NULL;
+    return t->token == NULL;
+}
+
+static void tokeniser_advance(Tokeniser* t)
+{
+    ASSERT(!tokeniser_eof(t), "next() on empty tokeniser");
+
+    skipSpace(t);
+    if (*t->cursor == 0) {
+        t->token = NULL;
+        return;
     }
 
     // These must be single chars, but we specify them as strings to we can
@@ -39,11 +50,12 @@ static const char* tokeniser_next(Tokeniser* t)
     for (int i = 0; i < ARRAY_SIZE(punctChars); i++) {
         if (*t->cursor == punctChars[i][0]) {
             t->cursor++;
-            return punctChars[i];
+            t->token = punctChars[i];
+            return;
         }
     }
 
-    const char* ret = t->cursor;
+    t->token = t->cursor;
     if (*t->cursor == '"') {
         // Scan out a string.
         *t->cursor = ':'; //XXX: TEMPORARY
@@ -53,7 +65,7 @@ static const char* tokeniser_next(Tokeniser* t)
                 // Clobber the closing "
                 *wp = 0;
                 t->cursor++;
-                return ret;
+                return;
 
             case '\\':
                 t->cursor++;
@@ -78,16 +90,74 @@ static const char* tokeniser_next(Tokeniser* t)
         for (t->cursor++; *t->cursor != 0; t->cursor++) {
             if (isspace(*t->cursor)) {
                 *t->cursor++ = 0;
-                return ret;
+                return;
             }
             switch (*t->cursor) {
             case '(': case ')': case '\'': case ' ': case '\t':
                 *t->cursor++ = 0;
-                return ret;
+                return;
             }
         }
     }
-    return ret;
+    return;
+}
+
+static const char* tokeniser_peek(Tokeniser* t)
+{
+    ASSERT(!tokeniser_eof(t), "peek() on empty tokeniser");
+    return t->token;
+}
+
+static const char* tokeniser_next(Tokeniser* t)
+{
+    ASSERT(!tokeniser_eof(t), "next() on empty tokeniser");
+    const char* token = t->token;
+    tokeniser_advance(t);
+    return token;
+}
+
+static void tokeniser_init(Tokeniser* t, char* input)
+{
+    t->cursor = input;
+    t->token  = input;
+    tokeniser_advance(t);
+}
+static int readInteger(const char* t, int* value)
+{
+    int sign = 1;
+    switch (*t) {
+        case '+': t++; break;
+        case '-': sign = -1; t++; break;
+        default: break;
+    }
+
+    *value = 0;
+    for ( ; *t != 0; t++) {
+        if (isdigit(*t)) {
+            *value = (*value * 10) + (*t - '0');
+        }
+        else {
+            return 0;
+        }
+    }
+    return *value * sign;
+}
+
+static Pointer readForm(Tokeniser* t);
+static Pointer readList(Tokeniser* t)
+{
+    if (tokeniser_eof(t)) {
+        throw("Expected ), got EOF");
+    }
+
+    const char* token = tokeniser_peek(t);
+    if (strEq(token, ")")) {
+        tokeniser_next(t);
+        return nil_make();
+    }
+
+    Pointer car = readForm(t);
+    return pair_make(car, readList(t));
 }
 
 static Pointer readForm(Tokeniser* t)
@@ -96,12 +166,25 @@ static Pointer readForm(Tokeniser* t)
         return nil_make();
     }
 
-    Pointer string = string_make(tokeniser_next(t));
-    return pair_make(string, readForm(t));
+    const char* token = tokeniser_next(t);
+
+    if (strEq(token, "(")) {
+        return readList(t);
+    }
+    if (token[0] == '"') {
+        return string_make(token + 1); // strip the leading quote
+    }
+    int intValue;
+    if (readInteger(token, &intValue)) {
+        return integer_make(intValue);
+    }
+    //TODO: this should be a symbol
+    return string_make(token);
 }
 
 Pointer readLine(char* input)
 {
-    Tokeniser t = { input, input };
+    Tokeniser t;
+    tokeniser_init(&t, input);
     return readForm(&t);
 }
